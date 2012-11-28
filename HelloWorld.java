160828755008d2aa83fa21a43d81bd10624ebb3e
Manraj G <manraj@skavasystems.com>
2012-11-28 23:26:52 +0530

    Modified java file

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,13 @@ public class HelloWorld
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+
+	    if (args != null && args.length > 0)
+        {
+            for (int i = 0; i < args.length; i++)
+            {
+                System.out.println("args[" + i + "]: " + args[i]);
+            }
+        }
     }
 }
